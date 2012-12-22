0769229af519d83cf21ab1800da4f98fa0cceba0
Tom Walker <tpwalke2@gmail.com>
2012-12-21 21:52:15 -0500

    Rebranding default site.

--- site/sitespecific/config.php
+++ site/sitespecific/config.php
@@ -1,16 +1,13 @@
 <?php
-  $walkercms_config['organization_name'] = 'Quickill Pest Services';
-  $walkercms_config['organization_full_title'] = 'Quickill Pest Services, Inc.';
-  $walkercms_config['description'] = 'Quickill Pest Services is a family-owned and operated pest control company that has been serving Fuquay-Varina and surrounding areas for over 25 years.';
-  $walkercms_config['keywords'] = 'Quickill pest control exterminator extermination Fuquay Varina Donald Dickens roach spider ant bee';
+  $walkercms_config['organization_name'] = 'WalkerCMS';
+  $walkercms_config['organization_full_title'] = 'WalkerCMS';
+  $walkercms_config['description'] = 'WalkerCMS is a file-based content-management system (CMS) that is suited primarily for small websites.';
+  $walkercms_config['keywords'] = 'content management system walkercms cms php laravel';
 
   $pageDefaults['perform_caching'] = false;
   $pageDefaults['show_in_nav'] = true;
 
-  $pages['home']     = array('menu_title' => 'Home');
-  $pages['services'] = array('page_title' => 'Offered Services', 'menu_title' => 'Our Services');
-  $pages['pests']    = array('page_title' => 'Common Pests', 'menu_title' => 'Common Pests');
-  $pages['wildlife'] = array('page_title' => 'Wildlife', 'menu_title' => 'Wildlife Management', 'external_url' => 'http://www.southernwildlifeandland.com/');
-  $pages['contact']  = array('page_title' => 'Contact Us', 'menu_title' => 'Contact Us');
-  $pages['about']    = array('page_title' => 'About Us', 'menu_title' => 'About Us');
+  $pages['home']          = array('menu_title' => 'Home');
+  $pages['documentation'] = array('menu_title' => 'Documentation');
+  $pages['source']        = array('menu_title' => 'Source', 'external_url' => 'https://github.com/tpwalke2/WalkerCMS');
 ?>
