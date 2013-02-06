c959898ff2a9111c022eb30e7a2a8a353be4c0a4
Tom Walker <tpwalke2@gmail.com>
2013-02-06 06:34:30 -0500

    #35 XHTML validation fixes

--- WalkerCMS/views/layouts/common.blade.php
+++ WalkerCMS/views/layouts/common.blade.php
@@ -23,22 +23,22 @@
   <meta name="copyright" content="Copyright &copy; {{ date('Y') }} by {{ $organization_full_title }} All Rights Reserved." />
   <meta name="description" content="{{ $site_description }}" />
   <meta name="keywords" content="{{ $site_keywords }}" />
-  {{ HTML::style('/styles/default.css') }}
-  {{ HTML::style('/styles/site.css') }}
-  {{ HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js') }}
-  {{ HTML::style("/scripts/fancybox/jquery.fancybox.css") }}
-  {{ HTML::script('/scripts/fancybox/jquery.fancybox.pack.js') }}
-  {{ HTML::script('/scripts/jquery.validate.min.js') }}
-  {{ HTML::script('/scripts/jquery.form.min.js') }}
-  {{ HTML::script('/scripts/default.js') }}
+  <link href="/styles/default.css" media="all" type="text/css" rel="stylesheet" />
+  <link href="/styles/site.css" media="all" type="text/css" rel="stylesheet" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>
+  <link href="/scripts/fancybox/jquery.fancybox.css" media="all" type="text/css" rel="stylesheet" />
+  <script src="/scripts/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+  <script src="/scripts/jquery.validate.min.js" type="text/javascript"></script>
+  <script src="/scripts/jquery.form.min.js" type="text/javascript"></script>
+  <script src="/scripts/default.js" type="text/javascript"></script>
   @if ($has_page_specific_html_header)
    {{ $page_specific_html_header }}
   @endif
   @if ($has_page_specific_stylesheet)
-   {{ HTML::style("/styles/${page_id}.css") }}
+   <link href="/styles/{{$page_id}}.css" media="all" type="text/css" rel="stylesheet" />
   @endif
   @if ($has_page_specific_javascript)
-   {{ HTML::script("/scripts/${page_id}.js") }}
+   <script src="/scripts/{{$page_id}}.js" type="text/javascript"></script>
   @endif
  </head>
  <body>
